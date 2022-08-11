22e0f6256c1bbc2baa99453c0633b0216c23a41a
rezneacanatolie <rezneacanatolie@gmail.com>
2022-08-11 14:05:49 +0300

    Fix issue with Square Screen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,21 +7,41 @@ const SquareScreen = () => {
     const [blue, setBlue] = useState(0);
     const [green, setGreen] = useState(0);
 
+    const Colour_Increment = 15;
+
+    const setColour = (colour, change) => {
+        switch (colour) {
+            case "red":
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
+                return;
+            case "blue":
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+                return;
+            case "green":
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+                return;
+            default:
+                return;
+
+        }
+
+    };
+
     return (
         <View>
             <ColourCounter
-                onIncrease={() => setRed(red + 1)}
-                onDecrease={() => setRed(red - 1)}
+                onIncrease={() => setColour("red", Colour_Increment)}
+                onDecrease={() => setColour("red", -1 * Colour_Increment)}
                 colour="Red" />
             <ColourCounter
-                onIncrease={() => setBlue(blue + 1)}
-                onDecrease={() => setBlue(blue - 1)}
+                onIncrease={() => setColour("blue", Colour_Increment)}
+                onDecrease={() => setColour("blue", -1 * Colour_Increment)}
                 colour="Blue" />
             <ColourCounter
-                onIncrease={() => setGreen(green + 1)}
-                onDecrease={() => setGreen(green - 1)}
+                onIncrease={() => setColour("green", Colour_Increment)}
+                onDecrease={() => setColour("green", -1 * Colour_Increment)}
                 colour="Green" />
-            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue}` }} />
+            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} />
         </View>
     );
 };
